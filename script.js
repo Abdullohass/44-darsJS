f5e9bae9df2e307465b4a6ad6c9210eda2657af4
Abdullohass <abdulloh.s.gk@gmail.com>
2024-09-10 15:31:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log(Math.PI)
+// console.log(Math.E)
+// console.log(Math.max(56,6,16,324,34234,5665,678,76597))
+// console.log(Math.min(56,6,16,324,34234,5665,678,76597))
+// console.log(Math.floor(4.99))
+// console.log(Math.ceil(4.000000001))
+// console.log(Math.random())
+
+// let a = 5;
+// if (a > 5) {
+//     alert("Mars da oqishingiz mumkun")
+// } else {
+//     alert9("Marsda oqishingiz mumkun mas")
+// }
+
+let randomSon = Math.floor(10 + Math.random() * 90)
+
+console.log(randomSon);
+
+alert("Bizda random son chiqishni boshlaydi!!!😊");
+alert("Random son:" + randomSon);
+confirm("Biz bu sonni 2ga kopaytirib bolmoqchimiz!!!");
+alert(`Random sonnimiz ${randomSon}. \nRandom sonni 2ga kopaytirdik:${randomSon * 2} \nRandom sonni 2ga boldik:${randomSon / 2}\nRandom sonni 3ga qoldiqa boldik :${randomSon % 3}`)
+
+
+
+
+
+
+
+
